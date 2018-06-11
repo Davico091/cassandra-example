b4ef670d993c453fc85e69d4d4766cb85ff1e465
David <perzc.david@gmail.com>
2018-06-10 19:45:09 -0500

    First commit - adding files

---
+++ src/main/java/com/example/cassandra/cassandraexample/CassandraExampleApplication.java
@@ -0,0 +1,12 @@
+package com.example.cassandra.cassandraexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassandraExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassandraExampleApplication.class, args);
+	}
+}
